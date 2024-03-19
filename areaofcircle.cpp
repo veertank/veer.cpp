86f6a362157ea728c2c0be833baf6db06c2fbd16
veertank <162274579+veertank@users.noreply.github.com>
2024-03-19 16:30:35 +0530

    Create areaofcircle.cpp

---
+++ areaofcircle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int radius =4;
+const float PI =3.14;
+double area;
+area=PI*radius *radius ;
+cout<<"area of circle"<<area;
+    return 0;
+}
